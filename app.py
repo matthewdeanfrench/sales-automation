85ba355a79257b6c4faeb80882ed1ce6dd0b4e86
Matt French <matthew.dean.french@gmail.com>
2026-03-16 13:50:29 -0700

    upgrade to PostgreSQL

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///claritycare.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///claritycare.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
